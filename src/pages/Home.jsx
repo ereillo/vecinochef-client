3d0bf7db23688cc71edcac2922fd1f2377cf0daf
ereillo <evelioreillo@gmail.com>
2023-09-04 17:37:19 +0200

    último push Jorge

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import { useContext, useEffect, useState } from "react";
+import AddEspecialidad from "../pages/AddEspecialidad";
+import axios from "axios"
+import { Link, useNavigate } from "react-router-dom";
+import service from "../services/service.config";
+import { AuthContext } from "../context/auth.context"
 
 function Home() {
   return (
--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -8,6 +8,7 @@ import { AuthContext } from "../context/auth.context"
 function MyProfile() {
 
   const [allEspecialidades, setAllEspecialidades] = useState()
+  const [allMenus, setAllMenus] = useState()
   const { activeUserId } = useContext(AuthContext);
   const navigate = useNavigate()
 
@@ -20,40 +21,67 @@ function MyProfile() {
   const getData = async () => {
 
     try {
-      const response = await service.get(`/user/myprofile`)
-      console.log(response.data)
-      setAllEspecialidades(response.data)
+      const especialidadesResponse = await service.get("/user/myprofile");
+      console.log(especialidadesResponse.data);
+      setAllEspecialidades(especialidadesResponse.data);
     } catch (error) {
-      console.log(error)
-      navigate("/error")
+      console.log(error);
+      navigate("/error");
     }
+    
+    try {
+      const menusResponse = await service.get("/menu/myprofile");
+      console.log(menusResponse.data);
+      setAllMenus(menusResponse.data);
+    } catch (error) {
+      console.log(error);
+      navigate("/error");
+    }
+    
   }
 
   return (
     <div>
-    <Link to={`/user/edit-profile/${activeUserId}`}>Edita tu perfil</Link>
-    {/* <AddEspecialidad getData={getData} setAllEspecialidades = {setAllEspecialidades}/> */}
-     <br/>
-     <hr/>
-     <h3>Lista de Especialidades</h3>
-     {allEspecialidades === undefined
-      ? <h3>... buscando</h3>
-      : allEspecialidades.map((eachEspecialidad) => {
-        return (
-          <>
-          <div key={eachEspecialidad._id}>
-            <Link to={`/esp/edit-especialidad/${eachEspecialidad._id}`}>{eachEspecialidad.especialidadNombre}</Link>
-            <br/>
-            <img src={eachEspecialidad.especialidadPic} width="150" />
-            <br/>
-            {/* <Link to={`/esp/edit-especialidad/${eachEspecialidad._id}`}>{eachEspecialidad.creador[0].userName}</Link> */}
-          </div>
-          </>
-        )
-      })
-      }
+      <Link to={`/user/edit-profile/${activeUserId}`}>Edita tu perfil</Link>
+      {/* <AddEspecialidad getData={getData} setAllEspecialidades={setAllEspecialidades}/> */}
+      <br />
+      <hr />
+      <div>
+        <h3>Lista de Especialidades</h3>
+        {allEspecialidades === undefined ? (
+          <h3>... buscando</h3>
+        ) : (
+          allEspecialidades.map((eachEspecialidad) => (
+            <div key={eachEspecialidad._id}>
+              <Link to={`/esp/edit-especialidad/${eachEspecialidad._id}`}>
+                {eachEspecialidad.especialidadNombre}
+              </Link>
+              <br />
+              <img src={eachEspecialidad.especialidadPic} width="150" alt={eachEspecialidad.especialidadNombre} />
+              <br />
+              {/* <Link to={`/esp/edit-especialidad/${eachEspecialidad._id}`}>{eachEspecialidad.creador[0].userName}</Link> */}
+            </div>
+          ))
+        )}
+      </div>
+      <hr />
+      <div>
+        <h3>Lista de Menús</h3>
+        {allMenus === undefined ? (
+          <h3>... buscando</h3>
+        ) : (
+          allMenus.map((eachMenu) => (
+            <div key={eachMenu._id}>
+              <Link to={`/esp/edit-menu/${eachMenu._id}`}>
+                {eachMenu.platoNombre}
+              </Link>
+              <br />
+            </div>
+          ))
+        )}
+      </div>
     </div>
-  )
-}
+  );
+  }
 
 export default MyProfile
